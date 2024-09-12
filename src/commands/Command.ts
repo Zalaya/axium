33b459b956daf02232c4a72cb18648e6aa517d3c
zalaya <enol.corzal@gmail.com>
2024-09-12 20:41:20 +0200

    feat(command): create the interface for the future commands on the moderation bot

---
+++ src/commands/Command.ts
@@ -0,0 +1,11 @@
+import {ApplicationCommandOptionData, Client, CommandInteraction} from "discord.js";
+
+export interface Command {
+
+    name: string;
+    description: string;
+    options: ApplicationCommandOptionData[];
+
+    execute(interaction: CommandInteraction, client: Client): Promise<void>;
+
+}
