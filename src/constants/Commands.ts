d1a171186fc4b6bf8e8ad3eb5cf69051a9b564a4
zalaya <enol.corzal@gmail.com>
2024-09-14 14:50:41 +0200

    refactor(command): remove the kick command as it was not working

--- src/constants/Commands.ts
+++ src/constants/Commands.ts
@@ -1,10 +1,8 @@
 import { Command } from "../interfaces/Command";
 import { CleanCommand } from "../commands/CleanCommand";
-import { KickCommand } from "../commands/KickCommand";
 
 export const commands: Command[] = [
 
-    new CleanCommand(),
-    new KickCommand()
+    new CleanCommand()
 
 ]
