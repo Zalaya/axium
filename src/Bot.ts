cf5be2afc6d9a19302a981d23fd9dbd784652e8e
zalaya <enol.corzal@gmail.com>
2024-09-15 16:22:25 +0200

    refactor(prettier): change the name of the registrars, they were called services, now a valid name in our program

--- src/Bot.ts
+++ src/Bot.ts
@@ -6,20 +6,20 @@ import { CommandRegistrar } from "./commands/CommandRegistrar";
 export class Bot {
 
     private readonly client: Client;
-    private readonly commandService: CommandRegistrar;
-    private readonly eventService: EventRegistrar;
+    private readonly commandRegistrar: CommandRegistrar;
+    private readonly eventRegistrar: EventRegistrar;
 
     public constructor() {
         this.client = new Client({ intents: intents });
-        this.commandService = new CommandRegistrar(this.client);
-        this.eventService = new EventRegistrar(this.client);
+        this.commandRegistrar = new CommandRegistrar(this.client);
+        this.eventRegistrar = new EventRegistrar(this.client);
     }
 
     public async start(): Promise<void> {
         try {
             await this.client.login(process.env.BOT_TOKEN);
-            this.eventService.registerEvents();
-            await this.commandService.registerCommands();
+            this.eventRegistrar.registerEvents();
+            await this.commandRegistrar.registerCommands();
         } catch (error) {
             console.error("An error occurred while trying to start the bot: ", error);
         }
