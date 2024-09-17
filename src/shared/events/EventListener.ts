806c550830f929647884b2d66d2ab546c4c4cb59
zalaya <enol.corzal@gmail.com>
2024-09-18 00:03:52 +0200

    refactor(listeners): change the responsibility of the event listeners, they knew about the event handlers and guardians, this is not ok, so we add an abstraction layer as a pipeline for the listeners to unknown the specifications

--- src/shared/events/EventListener.ts
+++ src/shared/events/EventListener.ts
@@ -1,22 +1,21 @@
-import { Guardian } from "../interfaces/Guardian";
 import { Client } from "discord.js";
-import { Handler } from "../interfaces/Handler";
 import { EventPipeline } from "./EventPipeline";
 
 export abstract class EventListener {
 
     protected abstract readonly name: string;
     protected abstract readonly once: boolean;
-    protected abstract readonly guardians: Guardian[];
-    protected abstract readonly handler: Handler;
+    protected abstract readonly pipeline: EventPipeline;
 
     public register(client: Client): void {
-        const pipeline = new EventPipeline(this.guardians, this.handler);
-
         if (this.once) {
-            client.once(this.name, async (...args: any[]) => await pipeline.process(...args));
+            client.once(this.name, async (...args: any[]) => {
+                await this.pipeline.process(...args);
+            });
         } else {
-            client.on(this.name, async (...args: any[]) => await pipeline.process(...args));
+            client.on(this.name, async (...args: any[]) => {
+                await this.pipeline.process(...args);
+            });
         }
     }
 
