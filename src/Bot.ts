409cd0b564c21fa1059556dab395779e42a75cec
zalaya <enol.corzal@gmail.com>
2024-09-17 17:15:47 +0200

    refactor(logs): change the way the logs are printed in the bot class, now we use string formatter utilities from javascript

--- src/Bot.ts
+++ src/Bot.ts
@@ -6,14 +6,16 @@ export class Bot {
     private readonly client: Client;
 
     public constructor() {
-        this.client = new Client({ intents: INTENTS });
+        this.client = new Client({
+            intents: INTENTS
+        });
     }
 
     public async start(): Promise<void> {
         try {
             await this.client.login(process.env.BOT_TOKEN);
         } catch (error) {
-            console.error("An error occurred while trying to start the bot: ", error);
+            console.error(`An error occurred while trying to start the bot: {error}`);
         }
     }
 
