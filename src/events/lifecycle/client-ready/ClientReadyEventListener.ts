9e95a1ca2d4533e9c1ac5283c958582aa959e674
zalaya <enol.corzal@gmail.com>
2024-09-20 11:25:08 +0200

    refactor(pipelines): change the listeners pipelines, now they will have only one pipeline, more than this is not necessary

--- src/events/lifecycle/client-ready/ClientReadyEventListener.ts
+++ src/events/lifecycle/client-ready/ClientReadyEventListener.ts
@@ -1,14 +1,12 @@
 import { EventListener } from "../../../shared/events/EventListener";
 import { Events } from "discord.js";
-import { ClientReadyEventHandler } from "./pipelines/ClientReadyEventHandler";
 import { EventPipeline } from "../../../shared/events/EventPipeline";
+import { ClientReadyEventHandler } from "./ClientReadyEventHandler";
 
 export class ClientReadyEventListener extends EventListener {
 
     protected readonly name = Events.ClientReady;
     protected readonly once = true;
-    protected readonly pipelines = [
-        new EventPipeline([], new ClientReadyEventHandler())
-    ];
+    protected readonly pipeline = new EventPipeline([], new ClientReadyEventHandler());
 
 }
--- src/shared/events/EventListener.ts
+++ src/shared/events/EventListener.ts
@@ -5,21 +5,15 @@ export abstract class EventListener {
 
     protected abstract readonly name: string;
     protected abstract readonly once: boolean;
-    protected abstract readonly pipelines: EventPipeline[];
+    protected abstract readonly pipeline: EventPipeline;
 
     public register(client: Client): void {
-        const notifier = async (...args: any[]) => await this.notify(...args);
+        const notifier = async (...args: any[]) => await this.pipeline.execute(...args);
 
         if (this.once) {
             client.once(this.name, notifier);
         } else {
             client.on(this.name, notifier);
-        }
-    }
-
-    private async notify(...args: any[]): Promise<void> {
-        for (const pipeline of this.pipelines) {
-            await pipeline.execute(...args);
         }
     }
 
